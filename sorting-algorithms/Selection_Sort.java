7f5dc2dcfb9cb9f74e3c0983ab1461471b1a31fa
lakmali-vishaka <119029100+lakmali-vishaka@users.noreply.github.com>
2024-10-30 19:05:54 +0530

    adding swapping element part

--- sorting-algorithms/Selection_Sort.java
+++ sorting-algorithms/Selection_Sort.java
@@ -8,6 +8,7 @@ Step 2: Swap the minimum element with the first unsorted element to place it in 
 Step 3: Repeat this process for the rest of the array until it is sorted. */
 
 /*Part 1: Find the Minimum Element */
+
 public class Selection_Sort {
 
     // Part 1: Find the minimum element index
@@ -20,5 +21,15 @@ public class Selection_Sort {
         }
         return minIndex;  // Return the index of the smallest element
     }
+
+    // Part 2: Swap two elements
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    
 }
+
 
