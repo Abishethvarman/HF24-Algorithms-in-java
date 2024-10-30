4065d76e84c3dd5583631525d76c0035ccdc3409
Abishethvarman V <57715922+Abishethvarman@users.noreply.github.com>
2024-10-30 18:52:02 +0530

    Update and rename Selection_Sort.java to Selection_Sort.md

--- sorting-algorithms/Selection_Sort.java
+++
@@ -1,3 +0,0 @@
-
-/*  Definition
-Selection Sort is a comparison-based sorting algorithm. It sorts an array by repeatedly selecting the smallest (or largest) element from the unsorted portion and swapping it with the first unsorted element. This process continues until the entire array is sorted..*/
